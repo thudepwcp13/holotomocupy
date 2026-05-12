2c6bdff0868a5773261ac13c35cd2b6a584270a6
Viktor Nikitin <vnikitin@anl.gov>
2026-05-12 15:54:56 -0500

    add bd reading in checkpoint

--- experimental/performance_tests/rec_iterative_mpi.py
+++ experimental/performance_tests/rec_iterative_mpi.py
@@ -55,7 +55,8 @@ logger.info("Read initial variables")
 ckpt = find_latest_checkpoint(args.path_out, args.start_iter)
 if ckpt:
     logger.info(f"Resuming from checkpoint: {ckpt}")
-    reader.read_checkpoint(ckpt, out_obj=cl.vars['obj'], out_pos=cl.vars['pos'], out_prb=cl.vars['prb'])
+    reader.read_checkpoint(ckpt, out_obj=cl.vars['obj'], out_pos=cl.vars['pos'], out_prb=cl.vars['prb'],
+                           out_bd=cl.vars.get('bd'))
 elif getattr(args, 'init_vol', None):
     logger.info(f"Reading initial object from vol file: {args.init_vol}")
     reader.read_vol_obj(args.init_vol, out=cl.vars["obj"], scale=getattr(args, "init_vol_scale", 1.0))
